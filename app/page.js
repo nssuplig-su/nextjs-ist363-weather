2571eaa665fd714eaf360e80b177a62e89e0d159
nssuplig-su <nssuplig@syr.edu>
2024-03-21 18:19:42 -0400

    attendance 03/21: react components and props

--- app/page.js
+++ app/page.js
@@ -1,5 +1,14 @@
+import Button from "../components/Button";
+
 const Homepage = () => {
-  return <div>Whooo</div>;
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <Button label="Download" />
+      <Button label="Register now" />
+      <Button label="Learn more" />
+    </div>
+  );
 };
 
 export default Homepage;
