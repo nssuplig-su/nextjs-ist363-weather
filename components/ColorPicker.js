1895f6af476d93aa0334abaf2904cacc47ea4a6e
nssuplig-su <nssuplig@syr.edu>
2024-03-28 18:26:16 -0400

    Attendance 03/28: Color Picker

--- components/ColorPicker.js
+++ components/ColorPicker.js
@@ -1,18 +1,60 @@
+import { useState } from "react";
+
+import Button from "./Button";
+
+import styles from "./ColorPicker.module.scss";
+
 const colors = [
   {
     name: "red",
     value: "#ff0000",
   },
+  {
+    name: "green",
+    value: "#00FF00",
+  },
+  {
+    name: "blue",
+    value: "#0000FF",
+  },
+  {
+    name: "yellow",
+    value: "#FFFF00",
+  },
+  {
+    name: "teal",
+    value: "#008080",
+  },
+  {
+    name: "orange",
+    value: "#FFA500",
+  },
 ];
 
 const ColorPicker = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
-    <div
-      style={{
-        backgroundColor: colors[0].value,
-      }}
-    >
-      Color picker
+    <div className={styles.colorpicker}>
+      <div
+        className={styles.colorpicker_block}
+        style={{
+          backgroundColor: colors[activeIndex].value,
+        }}
+      >
+        <h2 className={styles.colorpicker_name}>{colors[activeIndex].name}</h2>
+      </div>
+      {colors.map((color, index) => {
+        return (
+          <Button
+            key={index}
+            backgroundColor={color.value}
+            label={color.name}
+            clickHandler={() => {
+              setActiveIndex(index);
+            }}
+          />
+        );
+      })}
     </div>
   );
 };
